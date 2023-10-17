04e2df6ee5e79647db44edb0a9bec23341e790e3
Sai Teja Munduri <saitejamunduri.st@gmail.com>
2023-10-17 12:14:19 +0530

    Changes from main

---
+++ src/test/java/myntra_homefurnishings/TestCases/Search_test002.java
@@ -0,0 +1,34 @@
+package myntra_homefurnishings.TestCases;
+import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import resources.BaseTest;
+import resources.PageObjects;
+
+public class Search_test002 extends BaseTest
+{
+	@Test
+	public void searchValidation() throws IOException, InterruptedException
+	{
+		System.out.println("New Changes");
+		System.out.println("New changes 002");
+		initialize();
+		PageObjects elements= new PageObjects(driver);
+		elements.getSearchBar().sendKeys("Shirt");
+		List<WebElement> searchlist = elements.getSearchList();
+		for(WebElement item:searchlist)
+		{
+			String suggested_text = item.getText();
+			Assert.assertTrue(suggested_text.contains("Shirt"));
+		}
+		
+	}
+	@AfterMethod
+	public void closeBrowser()
+	{
+		driver.quit();;
+	}
+}
